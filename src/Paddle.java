577ab8f1205b95c31b2b9a2112dbb09ca0eb28cb
samee1528 <dineshkosala1528@gmail.com>
2022-05-13 13:24:45 +0530

    Pong Game Added

---
+++ src/Paddle.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Paddle {
+
+    Paddle(){
+
+
+    }
+
+    public void KeyPressed(KeyEvent e) {
+
+    }
+    public void KeyReleased(KeyEvent e) {
+
+    }
+    public void setYDirection(int yDirection){
+
+    }
+    public void move(){
+
+    }
+    public void draw(Graphics g) {
+
+    }
+}
